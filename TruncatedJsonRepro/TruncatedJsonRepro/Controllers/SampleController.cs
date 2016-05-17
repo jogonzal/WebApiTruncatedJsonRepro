e67d53bd095d7ff59e9f3482ff64ba9b586ffd45
Jorge <jogonzal@microsoft.com>
2016-05-17 14:01:32 -0700

    Make async, still no repro

--- TruncatedJsonRepro/TruncatedJsonRepro/Controllers/SampleController.cs
+++ TruncatedJsonRepro/TruncatedJsonRepro/Controllers/SampleController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 using Newtonsoft.Json;
@@ -13,19 +14,22 @@ namespace TruncatedJsonRepro.Controllers
 	public class SampleController : ApiController
 	{
 		// GET api/<controller>
-		public HttpResponseMessage Get()
+		public async Task<HttpResponseMessage> Get()
 		{
 			// REPRO's JSON truncation
+			await Task.Delay(100);
 			string myString = Resources.MyJson;
 			RootObject myRootObject = JsonConvert.DeserializeObject<RootObject>(myString);
+			Task.Delay(1000);
 			return Request.CreateResponse(HttpStatusCode.Created, myRootObject, new JsonMediaTypeFormatter());
 		}
 		
 		// POST api/<controller>
-		public HttpResponseMessage Post()
+		public async Task<HttpResponseMessage> Post()
 		{
 			// DOES NOT REPRO JSON truncation
 			string myString = Resources.MyJson;
+			await Task.Delay(100);
 			RootObject myRootObject = JsonConvert.DeserializeObject<RootObject>(myString);
 			string returnedString = JsonConvert.SerializeObject(myRootObject);
 			return new HttpResponseMessage(HttpStatusCode.Created)
